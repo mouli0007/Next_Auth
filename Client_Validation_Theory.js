a3565529df3d7ded8262e0341b233498b914aaec
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-17 18:22:02 +0530

    Update Client_Validation_Theory.js

--- Client_Validation_Theory.js
+++ Client_Validation_Theory.js
@@ -1,4 +1,5 @@
 
+
 ############### Next Auth client Side Validation ################
 
 import { signIn } from "next-auth/client";
@@ -20,11 +21,25 @@ import { signIn } from "next-auth/client";
 
 #####################################################################
 	  
-	  import { useSession } from "next-auth/client";
-
+	  ! important note :  Next_auth will automatically provised cookies (session token ) aka (JWT) 
+                    and it helps in changing the state (ex: When the user logs in provides validation for that ) and also 
+					helps when we try to send request to protected resource
+					
+	 ###### import { useSession } from "next-auth/client"; 
 	  
+     ### UseSession Hook can be used in any functional react component
+	 
+	 
 	  #### Syntax #####
 	  
 	    const [session, loading] = useSession();
 
 			With conditional rendering we can able to manage our state depends on uor session
+			
+			
+	##### import { signOut} from "next-auth/client"; 
+	
+	### SingOut Funciton will automatically Clears the Logged in client cookie(active session token cookie)
+	    But You can Still login with the user credentials 
+		the signout function will just clear the active session cookie so that we can do any kind
+		of conditional rendering if the cookie is presented or id its not
