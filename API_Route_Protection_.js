24544379e18fd88046d1f57e237f4685ab332e98
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-18 20:34:17 +0530

    Create API_Route_Protection_.js

---
+++ API_Route_Protection_.js
@@ -0,0 +1,7 @@
+
+		   ############## Api Route Protecion #################
+		   
+		   
+		   Lest take a scenerio for example if you dont want to every user to change their password
+		   u want to allow only authenticated users to do it 
+		   for that we need to protect our API Route
